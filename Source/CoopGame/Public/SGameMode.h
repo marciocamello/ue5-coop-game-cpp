b5abae39da2ea57ece2391bbce849954309094f4
marcio vale <mac3designer@gmail.com>
2022-11-29 17:07:04 +0000

    Create gamemode class Setup EQS Spawn AI using EQS

---
+++ Source/CoopGame/Public/SGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPGAME_API ASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
